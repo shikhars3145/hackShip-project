ff3ac62d933e9dd666c9ac50ac412e79133d360c
xANARKYx <shikhars3145@gmail.com>
2020-07-18 14:29:50 +0530

    added player movement

--- src/main.py
+++ src/main.py
@@ -4,9 +4,9 @@ import os
 # Paths
 current_path = os.path.dirname(__file__)  # path to main.py
 
-assets_path = os.path.join(current_path, 'assets')  # the asssets folder path
+assets_path = os.path.join(current_path, "assets")  # the asssets folder path
 
-images_path = os.path.join(assets_path, 'images')  # the images folder path
+images_path = os.path.join(assets_path, "images")  # the images folder path
 
 # Initialize the pygame
 pygame.init()
@@ -17,15 +17,16 @@ screen = pygame.display.set_mode((800, 600))
 
 
 # Title and Icon
-pygame.display.set_caption('hackShip project')
-icon = pygame.image.load((os.path.join(images_path, 'icon.png')))
+pygame.display.set_caption("hackShip project")
+icon = pygame.image.load((os.path.join(images_path, "icon.png")))
 pygame.display.set_icon(icon)
 
 
 # Player
-playerImg = pygame.image.load(os.path.join(images_path, 'player.png'))
+playerImg = pygame.image.load(os.path.join(images_path, "player.png"))
 playerX = 30
 playerY = 268
+playerY_Change = 0
 
 
 def player(x, y):
@@ -35,9 +36,23 @@ def player(x, y):
 # Game Loop
 RUNNING = True
 while RUNNING:
+
+    screen.fill((0, 0, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             RUNNING = False
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                playerY_Change = -0.1
+            if event.key == pygame.K_DOWN:
+                playerY_Change = 0.1
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                playerY_Change = 0
+
+    playerY += playerY_Change
     player(playerX, playerY)
     pygame.display.update()

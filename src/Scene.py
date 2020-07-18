3ce0cc6b155186bb127e2a3e32e57ce4da0fb3c6
Donghyeon Kim <0916dhkim@gmail.com>
2020-07-18 17:59:52 +0900

    Add Scene Class

---
+++ src/Scene.py
@@ -0,0 +1,7 @@
+from GameState import GameState
+
+# Class responsible for rendering stuff on screen.
+class Scene:
+    def render(self, state: GameState):
+        """Render contents on screen."""
+        pass

b4db25f5c97efc3576e24fd38a6de234ca253ef6
Donghyeon Kim <0916dhkim@gmail.com>
2020-07-18 18:43:32 +0900

    Add Scene.nextScene Method

--- src/Scene.py
+++ src/Scene.py
@@ -7,3 +7,8 @@ class Scene:
     def render(self, screen):
         """Render contents on screen."""
         pass
+
+    def nextScene(self):
+        """Return scene object for next frame"""
+        # By default, do not transition to another scene.
+        return self

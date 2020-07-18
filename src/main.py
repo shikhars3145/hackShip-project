fa790d10bffac95ff817a9b7106358d6a1fa08f0
xANARKYx <shikhars3145@gmail.com>
2020-07-18 14:01:51 +0530

    images folder structure

--- src/main.py
+++ src/main.py
@@ -1,4 +1,12 @@
 import pygame
+import os
+
+# Paths
+current_path = os.path.dirname(__file__)  # path to main.py
+
+assets_path = os.path.join(current_path, 'assets')  # the asssets folder path
+
+images_path = os.path.join(assets_path, 'images')  # the images folder path
 
 # Initialize the pygame
 pygame.init()
@@ -10,8 +18,18 @@ screen = pygame.display.set_mode((800, 600))
 
 # Title and Icon
 pygame.display.set_caption('hackShip project')
-# icon = pygame.image.load('./scuba-diver.png')
-# pygame.display.set_icon(icon)
+icon = pygame.image.load((os.path.join(images_path, 'icon.png')))
+pygame.display.set_icon(icon)
+
+
+# Player
+playerImg = pygame.image.load(os.path.join(images_path, 'player.png'))
+playerX = 30
+playerY = 268
+
+
+def player(x, y):
+    screen.blit(playerImg, (x, y))
 
 
 # Game Loop
@@ -21,4 +39,5 @@ while RUNNING:
         if event.type == pygame.QUIT:
             RUNNING = False
 
+    player(playerX, playerY)
     pygame.display.update()

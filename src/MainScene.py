e2dbdda7b46cd11e8625382841dc97cd77de1b17
Donghyeon Kim <0916dhkim@gmail.com>
2020-07-19 16:50:49 +0900

    Adjust Difficulty as Score Increases

--- src/MainScene.py
+++ src/MainScene.py
@@ -18,7 +18,17 @@ PEACEFUL_LOOP = "src/assets/audio/bgmLoop.wav"
 PEACEFUL_LOOP_COUNT = 1
 DANGER_LOOP = "src/assets/audio/bgmLoopShark.wav"
 TRASH_BOTTLE = "src/assets/audio/trashBottle.wav"
+
+# Difficulty factors.
+INITIAL_SCROLL_SPEED = 100
+INITIAL_SHARK_SPAWN_RATE = 0.3
+INITIAL_GARBAGE_SPAWN_RATE = 2
+SCROLL_SPEED_MULTIPLIER = 0.001
+SHARK_SPAWN_RATE_MULTIPLIER = 0.001
+GARBAGE_SPAWN_RATE_MULTIPLIER = 0.001
 GARBAGE_BONUS_MULTIPLIER = 10
+GARBAGE_SPEED_DEVIATION = 50
+SHARK_SPEED_DEVIATION = 150
 
 
 class MainScene(Scene):
@@ -41,11 +51,11 @@ class MainScene(Scene):
 
         # Difficulty parameters
         # Scrolling speed
-        self.scrollSpeed = 100
+        self.scrollSpeed = INITIAL_SCROLL_SPEED
         # Average spawn rate of sharks (sharks / sec)
-        self.sharkSpawnRate = 0.3
+        self.sharkSpawnRate = INITIAL_SHARK_SPAWN_RATE
         # Average spawn rate of garbage (trash / sec)
-        self.garbageSpawnRate = 2
+        self.garbageSpawnRate = INITIAL_GARBAGE_SPAWN_RATE
 
         # Performance
         self.garbageCollected = 0
@@ -77,6 +87,19 @@ class MainScene(Scene):
     def score(self):
         timeElapsed = int((time_ns() - self.startTime) // 1e9)
         return timeElapsed + self.garbageCollected * GARBAGE_BONUS_MULTIPLIER
+
+    def adjustDifficulty(self):
+        """Adjust game difficulty as the score gets higher"""
+        score = self.score()
+        self.scrollSpeed = (
+            INITIAL_SCROLL_SPEED + score * SCROLL_SPEED_MULTIPLIER
+        )
+        self.sharkSpawnRate = (
+            INITIAL_SHARK_SPAWN_RATE + score * SHARK_SPAWN_RATE_MULTIPLIER
+        )
+        self.garbageSpawnRate = (
+            INITIAL_GARBAGE_SPAWN_RATE + score * GARBAGE_SPAWN_RATE_MULTIPLIER
+        )
 
     def handleEvent(self, event):
         # Player movement
@@ -113,11 +136,19 @@ class MainScene(Scene):
         delta = (currentTime - self.lastRendered) / 1e9
         self.lastRendered = currentTime
 
+        # Adjust difficulty.
+        self.adjustDifficulty()
+
         # Spawn garbage.
         if self.spawnGarbageAfter <= 0:
             self.garbageGroup.add(
                 Garbage(
-                    (X_UPPER_LIM, random.randint(Y_LOWER_LIM, Y_UPPER_LIM))
+                    (X_UPPER_LIM, random.randint(Y_LOWER_LIM, Y_UPPER_LIM)),
+                    (
+                        -self.scrollSpeed
+                        - random.random() * GARBAGE_SPEED_DEVIATION,
+                        0,
+                    ),
                 )
             )
             self.spawnGarbageAfter = random.random() / self.garbageSpawnRate
@@ -132,7 +163,15 @@ class MainScene(Scene):
             if self.spawnSharkAfter <= 0:
                 self.sharkGroup.add(
                     Shark(
-                        (X_UPPER_LIM, random.randint(Y_LOWER_LIM, Y_UPPER_LIM))
+                        (
+                            X_UPPER_LIM,
+                            random.randint(Y_LOWER_LIM, Y_UPPER_LIM),
+                        ),
+                        (
+                            -self.scrollSpeed
+                            - random.random() * SHARK_SPEED_DEVIATION,
+                            0,
+                        ),
                     )
                 )
                 self.spawnSharkAfter = random.random() / self.sharkSpawnRate

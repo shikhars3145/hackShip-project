a96a120fa403005fd0f6eba90c8bf97869c28e75
xANARKYx <shikhars3145@gmail.com>
2020-07-19 14:33:51 +0530

    added player boundary

--- src/Player.py
+++ src/Player.py
@@ -1,6 +1,7 @@
 import pygame
 from typing import Tuple
 from KinemeticBody import KinematicBody
+from config import SCREEN_HEIGHT, SCREEN_WIDTH
 
 
 # Player sprite.
@@ -15,4 +16,22 @@ class Player(KinematicBody):
         self.ACCEL_CONST = 2500
 
     def update(self, delta: float):
+
+        # Player Boundary
+        if self.position[0] < 32:
+            self.velocity = (0, self.velocity[1])
+            self.position = (32, self.position[1])
+
+        if self.position[0] > SCREEN_WIDTH - 32:
+            self.velocity = (0, self.velocity[1])
+            self.position = (SCREEN_WIDTH - 32, self.position[1])
+
+        if self.position[1] < 32:
+            self.velocity = (self.velocity[0], 0)
+            self.position = (self.position[0], 32)
+
+        if self.position[1] > SCREEN_HEIGHT - 32:
+            self.velocity = (self.velocity[0], 0)
+            self.position = (self.position[0], SCREEN_HEIGHT - 32)
+
         super().update(delta)

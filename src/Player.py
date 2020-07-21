5839158c87a24b1251f3c790eace3d33e4769bf2
Donghyeon Kim <0916dhkim@gmail.com>
2020-07-21 14:03:09 +0900

    Animate Player

--- src/Player.py
+++ src/Player.py
@@ -3,6 +3,13 @@ from typing import Tuple
 from KinemeticBody import KinematicBody
 from config import SCREEN_HEIGHT, SCREEN_WIDTH
 from resource import getResource
+from animation import splitImage
+
+
+FRAME_RATE = 10
+PLAYER_SPRITE_SHEET = pygame.image.load(
+    getResource("images/underwater-diving/player/player-swimming.png")
+)
 
 
 # Player sprite.
@@ -10,13 +17,20 @@ class Player(KinematicBody):
     def __init__(
         self, position: Tuple[float, float],
     ):
-        super().__init__(
-            pygame.image.load(getResource("images/player.png")), position,
-        )
+        self.frames = splitImage(PLAYER_SPRITE_SHEET, 1, 7)
+        self.currentFrame = 0
+        self.timeToNextFrame = 0
+        super().__init__(self.frames[0], position)
         self.damping_constant = 1
         self.ACCEL_CONST = 2500
 
     def update(self, delta: float):
+        # Animate.
+        self.timeToNextFrame -= delta
+        if self.timeToNextFrame <= 0:
+            self.currentFrame = (self.currentFrame + 1) % len(self.frames)
+            self.image = self.frames[self.currentFrame]
+            self.timeToNextFrame += 1 / FRAME_RATE
 
         # Player Boundary
         if self.position[0] < 32:
